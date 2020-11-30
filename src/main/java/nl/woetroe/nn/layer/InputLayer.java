e0c5c412d0e8091254118bc68c7d5bf810a41c7a
Ronald Bunk <me@azoraqua.com>
2020-11-30 16:38:00 +0100

    Update InputLayer.java

--- src/main/java/nl/woetroe/nn/layer/InputLayer.java
+++ src/main/java/nl/woetroe/nn/layer/InputLayer.java
@@ -6,6 +6,22 @@ import nl.woetroe.nn.neuron.Neuron;
 
 import java.util.Arrays;
 
+/**
+  Copyright (C) 2020-2021, Wouter Kistemaker.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 public final class InputLayer extends Layer {
 
     private static final long serialVersionUID = -6512736351539200069L;
